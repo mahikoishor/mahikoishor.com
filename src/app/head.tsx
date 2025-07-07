dbfeadfcd044cfbb3d24522f8d286274f2b7a7f7
Mahi Koishor <207034747+mahikoishor@users.noreply.github.com>
2025-07-07 15:21:50 +0600

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,9 +4,9 @@ export default function Head() {
       <meta charSet="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <title>AMM Mahi Kaishar</title>
+      <title>Mahi Koishor</title>
       <meta name="description" content="I'm a dedicated and passionate programmer with a love ❤ for turning ideas into functional reality. My journey in the world of programming began 5 years ago, and since then, I've been on an exciting quest to continuously learn and grow in this ever-evolving field."/>
-      <meta name="author" content="ammmahikaishar <ammmahikaishar@gmail.com"/>
+      <meta name="author" content="mahikoishor <mahikoishor@gmail.com"/>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
